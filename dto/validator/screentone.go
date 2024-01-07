6877abd3306783b6357a5ee46a251a8279240140
ShaDream <mogilnikovshadream@gmail.com>
2024-01-07 23:46:45 +0500

    rename screentone validator func

--- dto/validator/screentone.go
+++ dto/validator/screentone.go
@@ -5,7 +5,7 @@ import (
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 )
 
-func ValidateScreentone(settings dto.ScreentoneSettings) error {
+func ValidateScreentoneSettings(settings dto.ScreentoneSettings) error {
 	return validation.ValidateStruct(&settings,
 		validation.Field(&settings.DotSize, validation.Min(2), validation.Max(100)),
 	)
